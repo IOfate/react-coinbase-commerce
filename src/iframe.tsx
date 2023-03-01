bc66ea89e62f7e388afc0aa7e0d0133e181d7fd4
Johann-S <johann.servoire@gmail.com>
2023-03-01 16:03:27 +0100

    fix iframe custom metadata

--- src/iframe.tsx
+++ src/iframe.tsx
@@ -121,7 +121,7 @@ export class Iframe extends Component<Props, State> {
       cacheDisabled: disableCaching
     }
 
-    if (typeof customMetadata !== 'string') {
+    if (customMetadata && typeof customMetadata !== 'string') {
       console.error('Received customMetadata not of "string" type. Ignoring.')
     } else {
       params.custom = customMetadata
